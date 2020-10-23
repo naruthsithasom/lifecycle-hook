4429ebc0f17192d26d6fec8e649d27b37d8c3d68
naruthsithasom <ett0088@gmail.com>
2020-10-23 15:20:56 +0700

    component did mount update

--- src/App.js
+++ src/App.js
@@ -2,26 +2,15 @@ import React from 'react'
 import './App.css'
 import Article from './Component/Article'
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     console.log("[App.js] Constructor")
     super(props)
     this.state = {
-      isShow: true
+      isShow: true,
+      article: "Add text"
     }
-   this.onChangeVisible = this.onChangeVisible.bind(this)
+    this.onChangeVisible = this.onChangeVisible.bind(this)
   }
-
-  static getDerivedStateFromProps(props, state){
-    console.log("[App.js] Get Derived State From Props")
-    return state
-  }
-
-  onChangeVisible(){
-    this.setState(prevState => ({
-      isShow: !prevState.isShow 
-    }))
-  }
-
   // วิธีนี้ให้ใช้ Allow function จะได้ไม่ต้อง bind ใน constructor
   /*
   onChangeVisible = () => {
@@ -30,15 +19,42 @@ class App extends React.Component {
     }))
   }
   */
-  componentDidMount(){
+  static getDerivedStateFromProps(props, state) {
+    console.log("[App.js] Get Derived State From Props")
+    return state
+  }
+
+  onChangeVisible() {
+    this.setState(prevState => ({
+      isShow: !prevState.isShow
+    }))
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[App.js] should component update")
+    return true
+  }
+
+  getSnapshotBeforeUpdate(pervProps, prevState) {
+    console.log("[App.js] get snapshot for update")
+    return {
+      x: 0,
+      y: 0
+    }
+  }
+  componentDidUpdate(pervProps, prevState, snapshot) {
+    console.log("[App.js] componet did update", snapshot)
+  }
+  componentDidMount() {
     console.log("[App.js] Component Did Mount")
   }
-  render(){
+
+  render() {
     console.log("[App.js] Render")
     return (
       <div className="Cus-text-center">
         <button onClick={this.onChangeVisible}>ON/OFF</button>
-    {this.state.isShow ? <Article article="asdfasdfasdfadsfasdfadsfadfadfadfadfadfadfasdf สกดสหกดกหด" />:null}
+        <input value={this.state.article} onChange={(data) => this.setState({ article: data.target.value })} />
+        {this.state.isShow ? <Article article={this.state.article} /> : null}
       </div>
     )
   }
--- src/Component/Article.js
+++ src/Component/Article.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
 
 export class Article extends Component {
+  static getDerivedStateFromProps(props, state){
+    console.log("[Article.js] get derived state from props")
+    return state
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log("[Article.js] should component update")
+    return true
+  }
+
+  getSnapshotBeforeUpdate( pervProps, prevState){
+    console.log("[Article.js] get snapshot for update")
+    return { 
+      x: 0,
+      y: 0
+    }
+  }
+  componentDidUpdate(pervProps, prevState, snapshot){
+    console.log("[Article.js] component did update", snapshot)
+  }
   render() {
     console.log('[Article.js] Render')
     return (
-      <i>
-        {this.props.article}
-      </i>
+        <h1>{this.props.article}</h1>
     )
   }
 }
